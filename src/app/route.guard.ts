700b7faa353d2eafd5528a7bcefba53db1f5f567
umeshthapa12 <Dell@UMESH>
2021-02-27 06:35:02 +0545

    update

--- src/app/route.guard.ts
+++ src/app/route.guard.ts
@@ -1,14 +1,29 @@
+import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 
+@Injectable({
+    providedIn: 'root'
+  })
+
 export class AlwaysAuthGuard implements CanActivate {
-    // constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
     canActivate() {
         let data = JSON.parse(localStorage.getItem('logged'));
         if (data !== null) {
             if (data.content.role === 'admin') {
                 return true;
-            }
+            } 
+            // else {
+            //     return this.router.parseUrl('/404');
+            //     // return false;
+            // }
+
+            // if(data.content.role !== 'admin') {
+            //     return this.router.navigate(['/access-denied']);
+            // }
         }
+
+
     }
 }
