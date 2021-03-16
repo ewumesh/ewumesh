a68feeb7e4f676dd2c0d6379f7a4f66af7e1a283
umeshthapa12 <Dell@UMESH>
2021-03-16 11:41:27 +0545

    update_dialog_animator

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -4,6 +4,7 @@ import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { collectionInOut, listAnimation, rowsAnimation } from 'src/app/shred/animations/animations';
+import { ExtendedMatDialog } from 'src/app/shred/dialog-animator';
 import { AuthService } from '../authentication/auth.service';
 import { ProfileFormComponent } from './profile-form.component';
 import { ProfilePictureComponent } from './profile-image/profile-picture.component';
@@ -26,7 +27,8 @@ export class ProfileComponent implements OnInit, AfterViewInit, OnDestroy {
     constructor(
         private dialog: MatDialog,
         private fb: FormBuilder,
-        private authService: AuthService
+        private authService: AuthService,
+        private dialogUtil: ExtendedMatDialog
     ) {
         this.getListOfUsers();
     }
@@ -64,15 +66,16 @@ export class ProfileComponent implements OnInit, AfterViewInit, OnDestroy {
         });
     }
 
-    edit(data) {
+    edit(id: number) {
         let d = this.list.find(_ => _.key === this.loggedUserProfile.key);
         let instance: MatDialogRef<ProfileFormComponent, any>;
         instance = this.dialog.open(ProfileFormComponent, {
             width: '900px',
             data: d ? d : {},
             autoFocus: false,
-            hasBackdrop: false
         });
+
+        this.dialogUtil.animateBackdropClick(instance);
     }
 
     uplaodImage() {
